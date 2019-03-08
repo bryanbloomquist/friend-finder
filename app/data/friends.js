6c7392604936a3a5e8e33f7c3c8eac62845507e8
bloomy215 <bmbloomquist@gmail.com>
2019-03-08 14:23:51 -0600

    api push currently working

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,6 +3,22 @@ var friendsList = [
         name: "Bryan Bloomquist",
         photo: "https://bryanbloomquist.com/assets/images/bio-pic.jpg",
         scores: [5,4,1,1,1,5,1,5,1,5]
+    },{
+        name: "Simon Pegg",
+        photo: "https://m.media-amazon.com/images/M/MV5BNzMwODE1NjA3OV5BMl5BanBnXkFtZTgwNTY5MzM2OTE@._V1_UX214_CR0,0,214,317_AL_.jpg",
+        scores: [5,5,5,5,5,5,5,5,5,5]
+    },{
+        name: "Nicolas Cage",
+        photo: "https://prabook.com/web/show-photo.jpg?id=5032",
+        scores: [1,1,1,1,1,1,1,1,1,1]
+    },{
+        name: "Joss Whedon",
+        photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NTU2MzE2NTEyOTQ1Njc1/joss-whedon-17181746-1-402.jpg",
+        scores: [3,3,3,3,3,3,3,3,3,3]
+    },{
+        name: "Ryan Reynolds",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Deadpool_2_Japan_Premiere_Red_Carpet_Ryan_Reynolds_%28cropped%29.jpg/220px-Deadpool_2_Japan_Premiere_Red_Carpet_Ryan_Reynolds_%28cropped%29.jpg",
+        scores: [1,2,5,5,5,1,5,1,5,1]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-var friendsList = require("../data/friends.js")
+var friendsList = require("../data/friends")
 
 module.exports = function(app) {
 
@@ -6,20 +6,8 @@ module.exports = function(app) {
         return res.json(friendsList);
     });
 
-    // app.get("/api/friends/:friend", function(req, res) {
-    //     var chosen = req.params.character;
-    //     console.log(chosen);
-    //     for (var i = 0; i < friendsList.length; i++) {
-    //         if (chosen === friendsList[i].routeName) {
-    //             return res.json(friendsList[i]);
-    //         }
-    //     }
-    //     return res.json(false);
-    // });
-
     app.post("/api/friends", function(req, res) {
         var newFriend = req.body;
-        // newfriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
         console.log(newFriend);
         friendsList.push(newFriend);
         res.json(newFriend);
