1e66368179aebe0cdef4bb245d4f35871be96038
bloomy215 <bmbloomquist@gmail.com>
2019-03-11 01:07:17 -0500

    algorithm functioning, just need to get modal to pop up

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,8 +9,22 @@ module.exports = function(app) {
     app.post("/api/friends", function(req, res) {
         var newFriend = req.body;
         console.log(newFriend);
+        var compatability = 50;
+        var matchName;
+        var matchPhoto;
+        for (var i=0; i < friendsList.length; i++) {
+            var newCompatability = 0;
+            for (var x=0; x < friendsList[i].scores.length; x++) {
+                newCompatability += Math.abs(parseInt(newFriend.scores[x]) - parseInt(friendsList[i].scores[x]))
+            }
+            if (parseInt(newCompatability) < parseInt(compatability)) {
+                matchName = friendsList[i].name;
+                matchPhoto = friendsList[i].photo;
+                compatability = newCompatability;
+            }
+        }
         friendsList.push(newFriend);
-        res.json(newFriend);
+        res.json({matchName: matchName, matchPhoto: matchPhoto});
     });
     
 }
