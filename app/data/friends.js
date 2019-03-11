9bde8b76259297e1bcca45248f48dd21b20bc5c7
bloomy215 <bmbloomquist@gmail.com>
2019-03-11 15:27:00 -0500

    updated friends list

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,24 @@
 var friendsList = [
     {
-        name: "Bryan Bloomquist",
-        photo: "https://bryanbloomquist.com/assets/images/bio-pic.jpg",
-        scores: [5,4,1,1,1,5,1,5,1,5]
-    },{
-        name: "Simon Pegg",
-        photo: "https://m.media-amazon.com/images/M/MV5BNzMwODE1NjA3OV5BMl5BanBnXkFtZTgwNTY5MzM2OTE@._V1_UX214_CR0,0,214,317_AL_.jpg",
-        scores: [5,5,5,5,5,5,5,5,5,5]
-    },{
         name: "Nicolas Cage",
         photo: "https://prabook.com/web/show-photo.jpg?id=5032",
         scores: [1,1,1,1,1,1,1,1,1,1]
+    },{
+        name: "Felicia Day",
+        photo: "https://m.media-amazon.com/images/M/MV5BMTc4OTI4NzUxNF5BMl5BanBnXkFtZTcwNzg0NDgyMQ@@._V1_UY317_CR55,0,214,317_AL_.jpg",
+        scores: [2,2,2,2,2,2,2,2,2,2]
     },{
         name: "Joss Whedon",
         photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NTU2MzE2NTEyOTQ1Njc1/joss-whedon-17181746-1-402.jpg",
         scores: [3,3,3,3,3,3,3,3,3,3]
     },{
-        name: "Ryan Reynolds",
-        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Deadpool_2_Japan_Premiere_Red_Carpet_Ryan_Reynolds_%28cropped%29.jpg/220px-Deadpool_2_Japan_Premiere_Red_Carpet_Ryan_Reynolds_%28cropped%29.jpg",
-        scores: [1,2,5,5,5,1,5,1,5,1]
+        name: "Lindsey Stirling",
+        photo: "https://res.cloudinary.com/allamerican/image/fetch/t_face_s270/https://speakerdata2.s3.amazonaws.com/photo/image/866510/lindsey-stirling-famechangers.jpg",
+        scores: [4,4,4,4,4,4,4,4,4,4]
+    },{
+        name: "Simon Pegg",
+        photo: "https://m.media-amazon.com/images/M/MV5BNzMwODE1NjA3OV5BMl5BanBnXkFtZTgwNTY5MzM2OTE@._V1_UX214_CR0,0,214,317_AL_.jpg",
+        scores: [5,5,5,5,5,5,5,5,5,5]
     }
 ];
 
